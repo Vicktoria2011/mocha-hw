9b4f75a41f22db07d050871796ffe5fd1ddd79ec
Zakharova Victoria <partners@sportaq.com>
2023-10-16 10:16:38 +0300

    added hw 16.10.23

---
+++ cypress/e2e/hw-mocha.cy.js
@@ -0,0 +1,30 @@
+const logInData = [
+    { 
+      login: '727865319689', 
+      password: '123456',
+      expectedResult: 'eldorado' 
+    },
+    {
+      login: '094747973123', 
+      password: '123456',
+      expectedResult: 'eldorado' 
+    },
+    { 
+      login: '09ааа23', 
+      password: '123456',
+      expectedResult: 'eldorado' 
+    },
+  ];
+  
+  logInData.forEach((userData, index) => {
+    it(`Тест #${index + 1}: Вхід з логіном "${userData.login}" та паролем "${userData.password}"`, () => {
+      cy.visit('https://www.tubetgol.com/es/');
+  
+      cy.get('[class="btn btn-login"]').click();
+      cy.get('#login-input').type(userData.login); 
+      cy.get('#password-input').type(userData.password); 
+      cy.get('#login-button').click(); 
+  
+      cy.get('[data-cy="/aviator-route"]').click().should('be.visible'); 
+    });
+  });
